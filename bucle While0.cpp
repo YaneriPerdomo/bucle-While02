31b96afda449b037dd64203e87459fb82284ba1f
YaneriPerdomo <92409304+YaneriPerdomo@users.noreply.github.com>
2021-11-20 21:08:55 -0400

    Add files via upload

---
+++ bucle While0.cpp
@@ -0,0 +1,24 @@
+*/La sentencia while
+
+    while (Expresion logical) }
+         Conjunto de instrucciones;
+        }
+*/
+
+#include<iostream>
+#inclede<conio.h>
+using namespace std;
+
+int main(){
+    int i;
+
+    i=16;
+
+    while(i<=10){
+            cout<<1<<endl;
+            1--;
+
+    }
+    getch();
+    return 0;
+}
